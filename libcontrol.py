c0864a072ec239cb5f970cb02b1ca895b0189d4b
Soumyanil Das <dassic@outlook.com>
2022-02-08 19:15:53 +0530

    Added the imports

---
+++ libcontrol.py
@@ -0,0 +1,9 @@
+import argparse             # Helps in parsing command line arguments
+import collections          # Helps in providing more container types like OrderedDict
+import configparser         # Helps in reading and writing configuration files
+import hashlib              # For SHA
+import os
+import re
+import sys
+import zlib                 # Compressing files
+

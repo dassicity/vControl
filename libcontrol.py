4baa39dde0d18cfe1ec108fbbc81076165e93e04
Soumyanil Das <dassic@outlook.com>
2022-03-07 22:29:15 +0530

    Added Hash-object functionality

--- libcontrol.py
+++ libcontrol.py
@@ -19,11 +19,11 @@ def main(argv=sys.argv[1:]):
     args=argparser.parse_args(argv);
 
     if args.command == "add"            : cmd_add(args);
-    elif args.command == "cat-file"     : cmd_cat_file(args);
+    elif args.command == "cat-file"     : cmd_cat_file(args);       # Done
     elif args.command == "checkout"     : cmd_checkout(args);
     elif args.command == "commit"       : cmd_commit(args);
-    elif args.command == "hash-object"  : cmd_cat_file(args);
-    elif args.command == "init"          : cmd_init(args);
+    elif args.command == "hash-object"  : cmd_hash_object(args);    # Done
+    elif args.command == "init"         : cmd_init(args);           # Done
     elif args.command == "log"          : cmd_log(args);
     elif args.command == "ls-tree"      : cmd_ls_tree(args);
     elif args.command == "merge"        : cmd_merge(args);
@@ -241,3 +241,33 @@ def cmd_cat_file(args):
 def cat_file(repo, obj, fmt=None):
     obj = object_read(repo, object_find(repo, obj, fmt=fmt));
     sys.stdout.buffer.write(obj.serialize());
+
+"""Hash object command"""
+
+argsp = argsubparsers.add_parser("hash-object" , help="Compute hash of a file and either store it or print its hash");
+
+argsp.add_argument("-t" , metavar="type" , dest="type" , choices=["blob" , "commit" , "tag" , "tree"], default="blob" , help="Specify the type");
+argsp.add_argument("-w" , dest="write" , action="store_true" , help="Actually write the object into the database");
+argsp.add_argument("path" , help="Read object from <file>");
+
+def cmd_hash_object(args):
+    if args.write:
+        repo = GitRepository(".");
+    else:
+        repo = None;
+
+    with open(args.path, "rb") as fd:
+        sha = object_hash(fd, args.type.encode(), repo);
+        print(sha);
+
+def object_hash(fd, fmt, repo=None):
+    data=fd.read();
+
+    if fmt==b'commit' : obj=GitCommit(repo, data)
+    elif fmt==b'tree' : obj=GitTree(repo, data)
+    if fmt==b'tag' : obj=GitTag(repo, data)
+    if fmt==b'blob' : obj=GitBlob(repo, data)
+    else:
+        raise Exception("Unknown type %s!" % fmt)
+    
+    return object_write(obj , repo)
